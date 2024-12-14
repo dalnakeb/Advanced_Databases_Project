b87c943fb73c47286644435c728742c58f49f670
@coquerea <aristide.coquereau@grenoble-inp.org>
2024-12-14 17:24:19 +0100

    data querying cleaned

--- src/graphite/api_queries.py
+++ src/graphite/api_queries.py
@@ -2,15 +2,16 @@
 
 #!/usr/bin/python3
 import json
-import requests
 from urllib.parse import quote #inshallah ca passe
+import requests
 
 GRAPHITE_URL = "http://localhost/render"  # graphite render api's endpoint
 METRIC = "environment.air_pressure"
 
 #here, we only fetch 24 points (we have 1 per hour)
-FROM_TIME = "2020-01-01 00:00:00"       #these can be changed to fetch different amount of points
-UNTIL_TIME = "2020-02-01 00:00:00"
+#we use the unix epoch format (seconds since 1/1/1970)
+FROM_TIME = "1578675600"
+UNTIL_TIME = "1732024800"
 
 def query_graphite(metric, from_time, until_time):
     """make a query to graphite's render api over the given metric, 
@@ -20,8 +21,9 @@ def query_graphite(metric, from_time, until_time):
         "from": quote(from_time),
         "until": quote(until_time),
         "format": "json",
+        "noNullPoints": "true",
     }
-
+    print(f"Querying with params: {params}")
     try:
         # Send the GET request to Graphite
         response = requests.get(GRAPHITE_URL, params=params, timeout=10)
